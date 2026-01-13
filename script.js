9762c7a7af45678fe2e2b2799656fc2c41d2a725
DuongLx <duonglx@outlook.com>
2026-01-13 08:07:54 +0700

    fix: reduce scroll offset to 10px

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ function initSmoothScroll() {
 
       e.preventDefault();
 
-      // Fixed offset: 30px
-      const offset = 30;
+      // Fixed offset: 10px
+      const offset = 10;
       const targetPosition = target.offsetTop - offset;
 
       window.scrollTo({
